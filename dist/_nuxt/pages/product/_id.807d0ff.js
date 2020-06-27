35bf65d0c34c3d680ccb542a0e3dd751d1bc83a1
Smail <aouissa.smail@gmail.com>
2020-06-27 14:42:05 +0100

    wip

---
+++ dist/_nuxt/pages/product/_id.807d0ff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{336:function(t,n,e){"use strict";e.r(n);var c={components:{Product:e(328).default}},o=e(12),component=Object(o.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("product")}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ pages/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div> 404 </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+}
+</script>
