305db6da3d6e9044c1f80b3acfc718f31f0fe6cf
Bachir <bmnorredine@gmail.com>
2020-06-29 21:58:10 +0000

    change dimension logo footer

---
+++ dist/_nuxt/29a396b25769bcb35d39.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{324:function(t,n,e){"use strict";e.r(n);var c={components:{Product:e(317).default}},o=e(11),component=Object(o.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("product")}),[],!1,null,null,null);n.default=component.exports}}]);
