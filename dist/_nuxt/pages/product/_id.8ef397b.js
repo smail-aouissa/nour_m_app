6f6d06452eb669fbe9c26286c44c44d82705a3d6
Bachir <bmnorredine@gmail.com>
2020-06-29 11:53:31 +0000

    change en stock

--- dist/_nuxt/pages/product/_id.8ef397b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{338:function(t,n,e){"use strict";e.r(n);var c={components:{Product:e(331).default}},o=e(12),component=Object(o.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("product")}),[],!1,null,null,null);n.default=component.exports}}]);
